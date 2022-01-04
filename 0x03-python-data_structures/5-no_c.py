e4937874d04b786bdf8c632d2e94d6bbfd28f299
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-03 21:16:12 -0500

    task 5 attempt 9999

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     slist = list(my_string)
-    for i in slist:
+    for i in slist[0:]:
         if i == "c" or i == "C":
             slist.remove(i)
     return("".join(slist))
