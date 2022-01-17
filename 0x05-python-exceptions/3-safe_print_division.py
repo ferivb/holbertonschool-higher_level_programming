f90a4fa9372e17be2fdcdd4b7039e208b1c87196
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-17 15:50:23 -0500

    task 3 attempt 1

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quotient = None
+        quotient = a / b
+        return (quotient)
+    except ZeroDivisionError:
+        return (None)
+    finally:
+        print("Inside result: {}".format(quotient))
