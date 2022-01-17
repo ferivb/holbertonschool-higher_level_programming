4ded601656ae8c92b4455f639764193b76906e0e
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-17 13:22:46 -0500

    task 0 attempt 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
 	try:
 		new_list = my_list[:x]
 		count = 0
-		print(new_list)
+		print(*new_list, sep='')
 		for i in new_list:
 			count = count + 1
 		return count
