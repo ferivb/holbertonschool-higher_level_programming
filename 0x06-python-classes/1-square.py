969007e330d546fa12ffc7f714495ae20c371c93
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-18 18:46:32 -0500

    task 1 attempt 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square with a private size"""
+
+    def __init__(self, size):
+        """Instance attributes"""
+        self.__size = size
