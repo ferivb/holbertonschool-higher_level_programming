f4b07c21946b96aed82f949f1aaec510d8e799ab
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-18 19:02:30 -0500

    task 3 attempt 1

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square with a private size"""
+
+    def __init__(self, size=0):
+        """Instance attributes"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area of the Square Method"""
+        return self.__size * self.__size
