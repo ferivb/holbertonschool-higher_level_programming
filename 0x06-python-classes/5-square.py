c740a23fe50ba372074d8f74fa7bea4b882f9afc
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-19 12:51:30 -0500

    task 5 attempt 1

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square with a private size"""
+
+    def __init__(self, size=0):
+        """Initializes the square with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area of the Square Method"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Makes an attribute out of a method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value provided in main as the size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Public instance Method to print a square"""
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
