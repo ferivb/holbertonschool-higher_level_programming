c0d09b75a1a8ebbea4f95eebb8ddefee8e75032c
Felipe Rivas <fe.rivasb@gmail.com>
2022-02-01 22:26:21 -0500

    task 7 attempt 1

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that takes arguments and turns
+it into a list to the n save it in a file"""
+
+import json
+from sys import argv
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+" Create the object or empty list "
+try:
+    file = open(filename, 'r')
+    object = load_from_json_file(filename)
+except Exception:
+    object = []
+
+""" Fill in the list with the args """
+for i in range(1, len(argv)):
+    object.append(argv[i])
+
+" Dump it on the file "
+with open(filename, 'w') as file:
+    json.dump(object, file)
