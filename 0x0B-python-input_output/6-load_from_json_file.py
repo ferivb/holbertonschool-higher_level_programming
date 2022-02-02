8267fb8ac5430faca96b04c3eb043cdb7b90b7e3
Felipe Rivas <fe.rivasb@gmail.com>
2022-02-02 10:10:15 -0500

    task 6 added enconding

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Function that creates an Object from a “JSON file"""
-    with open(filename, 'r') as f:
-        return(json.load(f))
+    with open(filename, 'r', encoding='utf-8') as file:
+        return(json.load(file))
