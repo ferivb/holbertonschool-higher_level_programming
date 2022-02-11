056e3d98528733ae1ece0ee379c1a94e396609b4
Felipe Rivas <fe.rivasb@gmail.com>
2022-02-11 11:12:34 -0500

    task 17 attempt 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,10 @@ class Base:
             else:
                 list_dicts = [i.to_dictionary() for i in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
