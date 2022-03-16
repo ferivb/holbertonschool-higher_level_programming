c35d49779c5d352ca14ea585af84f23ba76b0968
Felipe Rivas <fe.rivasb@gmail.com>
2022-03-15 21:49:49 -0500

    task 9 attempt 2

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,12 @@
 -- Script that that creates a table called second_table in the current database
 -- and adds multiples rows.
 CREATE TABLE IF NOT EXISTS second_table(
-    id INT AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256),
-    score INT,
-    PRIMARY KEY (id)
+    score INT
 );
 
-INSERT INTO second_table(name, score) VALUES('John', 10);
-INSERT INTO second_table(name, score) VALUES('Alex', 3);
-INSERT INTO second_table(name, score) VALUES('Bob', 14);
-INSERT INTO second_table(name, score) VALUES('George', 8);
+INSERT INTO second_table VALUES(1, 'John', 10);
+INSERT INTO second_table VALUES(2, 'Alex', 3);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUES(4, 'George', 8);
