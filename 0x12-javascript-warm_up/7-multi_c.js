d3fd22bcb25b7e248d410e1cf90f8f869a36b403
Felipe Rivas <fe.rivasb@gmail.com>
2022-04-04 17:09:03 -0500

    task 7 attempt 1

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+if (isNaN(parseInt(args[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(args[2]); i++) {
+    console.log('C is fun');
+  }
+}
