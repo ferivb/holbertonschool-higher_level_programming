a631dd8d91b5b21330d04a2007e96b68da357ff3
Felipe Rivas <fe.rivasb@gmail.com>
2022-04-12 09:34:19 -0500

    task 4 semistandard correction

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 
 request(args[0], function (error, response, body) {
   if (error) {
-    console.error('error:', error); // Print the error if one occurred
+    console.error(error); // Print the error if one occurred
   }
   // console.log('statusCode:', response && response.statusCode); // Ion need this for now.
   const holder = JSON.parse(body);
@@ -13,7 +13,7 @@ request(args[0], function (error, response, body) {
   let count = 0;
   for (const title of film) {
     const chars = title.characters;
-    for (search of chars) {
+    for (const search of chars) {
       if (search === 'https://swapi-api.hbtn.io/api/people/18/') {
         count++;
       }
