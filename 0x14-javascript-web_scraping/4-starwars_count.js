71bf9346e39e0a5f8607e3a735fa71b7ceacc986
Felipe Rivas <fe.rivasb@gmail.com>
2022-04-12 09:45:10 -0500

    task 4 attempt 3

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,18 @@ const request = require('request');
 const args = process.argv.slice(2);
 
 request(args[0], function (error, response, body) {
-  if (error) {
-    console.error(error); // Print the error if one occurred
-  }
-  // console.log('statusCode:', response && response.statusCode); // Ion need this for now.
-  const holder = JSON.parse(body);
-  const film = holder.results;
-  let count = 0;
-  for (const title of film) {
-    const chars = title.characters;
-    for (const search of chars) {
-      if (search === 'https://swapi-api.hbtn.io/api/people/18/') {
-        count++;
+  if (!error) {
+    const holder = JSON.parse(body);
+    const film = holder.results;
+    let count = 0;
+    for (const title of film) {
+      const chars = title.characters;
+      for (const search of chars) {
+        if (search === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(count);
 });
